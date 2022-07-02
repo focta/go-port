48e67a7fff7b6692bb78770322c467c63aa612b1
focta <focke.wulf7234@gmail.com>
2022-07-03 07:58:35 +0900

    net/http の使い方を習得

--- main.go
+++ main.go
@@ -1,57 +1,78 @@
-// package main
-
-// import (
-// 	"log"
-
-// 	"github.com/gin-gonic/gin"
-// )
-
-// func main() {
-
-// 	log.Println("start server...")
-// 	r := gin.Default()
-
-// 	r.GET("/hello", func(context *gin.Context) {
-// 		context.JSON(200, gin.H{
-// 			"message": "Hello World!",
-// 		})
-// 	})
-
-// 	r.GET("/test", func(context *gin.Context) {
-// 		context.JSON(200, gin.H{
-// 			"message": "yeah!",
-// 		})
-// 	})
-
-// 	log.Fatal(r.Run())
-// }
 package main
 
 import (
-	"flag" // 追加する
 	"fmt"
-	"strconv"
+	"log"
+	"net/http"
 )
 
 func main() {
-	// コマンドラインインターフェースでの実行
-	flag.Parse()
-	args := flag.Args()
 
-	if len(args) == 0 {
+	// パラメータ受け取り
+	http.HandleFunc("/params", handleParams)
+
+	// POSTのみ許可.
+	http.HandleFunc("/onlyPost", handleOnlyPost)
+
+	// POSTのみ許可.
+	http.HandleFunc("/basicAuth", handleBasicAuth)
+
+	// 8080ポートで起動
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func handleOnlyPost(w http.ResponseWriter, r *http.Request) {
+
+	// HTTPメソッドをチェック（POSTのみ許可）
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed) // 405
+		w.Write([]byte("POSTだけだよー \n"))
+		w.Write([]byte(fmt.Sprintf("userAgent: %s \n", r.UserAgent())))
+		w.Write([]byte(fmt.Sprintf("userAgent: %s \n", r.UserAgent())))
 		return
-	} else {
-		fmt.Printf("Hello %s\n", args[0])
-		for i := range args {
-			fmt.Printf("key: %s \t", strconv.Itoa(i))
-			fmt.Printf("value: %s \n", args[i])
-		}
 	}
 
-	// ファイル作成と実行
-	arg := flag.Arg(0)
+	w.Write([]byte("OK"))
+}
 
-	msg := fmt.Sprintf("Hello %s\n", arg)
+func handleParams(w http.ResponseWriter, r *http.Request) {
 
-	fileWriter(msg)
+	// クエリパラメータ取得してみる
+	fmt.Fprintf(w, "クエリ：%s\n", r.URL.RawQuery)
+
+	// Bodyデータを扱う場合には、事前にパースを行う
+	r.ParseForm()
+
+	// Formデータを取得.
+	form := r.PostForm
+	fmt.Fprintf(w, "フォーム：\n%v\n", form)
+
+	// または、クエリパラメータも含めて全部.
+	params := r.Form
+	fmt.Fprintf(w, "フォーム2：\n%v\n", params)
+}
+
+func handleBasicAuth(w http.ResponseWriter, r *http.Request) {
+
+	// Basic認証のデータ取得.
+	username, password, ok := r.BasicAuth()
+
+	// そもそもそんなヘッダーがないなどのエラー.
+	if ok == false {
+		w.Header().Set("WWW-Authenticate", `Basic realm="SECRET AREA"`)
+		w.WriteHeader(http.StatusUnauthorized) // 401
+		fmt.Fprintf(w, "%d Not authorized.", http.StatusUnauthorized)
+		return
+	}
+
+	// Basic認証のヘッダーはあるけど、値が不正な場合.
+	if username != "my" || password != "secret" {
+		w.Header().Set("WWW-Authenticate", `Basic realm="SECRET AREA"`)
+		w.WriteHeader(http.StatusUnauthorized) // 401
+		fmt.Fprintf(w, "%d Not authorized.", http.StatusUnauthorized)
+		return
+	}
+
+	// OK
+	fmt.Fprint(w, "OK")
 }
