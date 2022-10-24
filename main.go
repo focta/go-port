69ab9c26c8d19f55da97fd012163b0d365280d57
focta <focke.wulf7234@gmail.com>
2022-10-25 02:53:41 +0900

    net/httpクラスでhttpサーバーを作り直してみる

--- main.go
+++ main.go
@@ -2,86 +2,29 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
+type User struct {
+	ID   int    `db:"id"`
+	Name string `db:"name"`
+	Age  int    `db:"age"`
+}
+
 func main() {
-
-	// パラメータ受け取り
-	http.HandleFunc("/params", handleParams)
-
-	// POSTのみ許可.
-	http.HandleFunc("/onlyPost", handleOnlyPost)
-
-	// POSTのみ許可.
-	http.HandleFunc("/basicAuth", handleBasicAuth)
-
-	// DBアクセスするパス
-	// http.HandleFunc("/dbAccess", handleDbAccess)
-
-	// 8080ポートで起動
-	log.Fatal(http.ListenAndServe(":8080", nil))
-}
-
-func handleOnlyPost(w http.ResponseWriter, r *http.Request) {
-
-	// HTTPメソッドをチェック（POSTのみ許可）
-	if r.Method != http.MethodPost {
-		w.WriteHeader(http.StatusMethodNotAllowed) // 405
-		w.Write([]byte("POSTだけだよー \n"))
-		w.Write([]byte(fmt.Sprintf("userAgent: %s \n", r.UserAgent())))
-		w.Write([]byte(fmt.Sprintf("userAgent: %s \n", r.UserAgent())))
-		return
+	err := http.ListenAndServe(
+		":18080",
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+		}),
+	)
+	if err != nil {
+		fmt.Printf("failed to terminate server: %v", err)
+		os.Exit(1)
 	}
-
-	w.Write([]byte("OK"))
-}
-
-func handleParams(w http.ResponseWriter, r *http.Request) {
-
-	// クエリパラメータ取得してみる
-	fmt.Fprintf(w, "クエリ：%s\n", r.URL.RawQuery)
-
-	// Bodyデータを扱う場合には、事前にパースを行う
-	r.ParseForm()
-
-	// Formデータを取得.
-	form := r.PostForm
-	fmt.Fprintf(w, "フォーム：\n%v\n", form)
-
-	// または、クエリパラメータも含めて全部.
-	params := r.Form
-	fmt.Fprintf(w, "フォーム2：\n%v\n", params)
-}
-
-func handleBasicAuth(w http.ResponseWriter, r *http.Request) {
-
-	// Basic認証のデータ取得.
-	username, password, ok := r.BasicAuth()
-
-	// そもそもそんなヘッダーがないなどのエラー.
-	if !ok {
-		w.Header().Set("WWW-Authenticate", `Basic realm="SECRET AREA"`)
-		w.WriteHeader(http.StatusUnauthorized) // 401
-		fmt.Fprintf(w, "%d Not authorized.", http.StatusUnauthorized)
-		return
-	}
-
-	// Basic認証のヘッダーはあるけど、値が不正な場合.
-	if username != "my" || password != "secret" {
-		w.Header().Set("WWW-Authenticate", `Basic realm="SECRET AREA"`)
-		w.WriteHeader(http.StatusUnauthorized) // 401
-		fmt.Fprintf(w, "%d Not authorized.", http.StatusUnauthorized)
-		return
-	}
-
-	// OK
-	fmt.Fprint(w, "OK")
-}
-
-func Add(a, b int) int {
-	return a + b
 }
 
 const englishHelloPrefix = "Hello, "
