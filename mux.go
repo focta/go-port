a1e99dcced73e8fe906268b381b58a306c76abce
focta <focke.wulf7234@gmail.com>
2022-12-07 06:57:26 +0900

    ルーティングを追加して、Requestを受け取るところまでを確認した

--- mux.go
+++ mux.go
@@ -1,12 +1,30 @@
 package main
 
-import "net/http"
+import (
+	"go-port/handler"
+	"go-port/store"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-playground/validator/v10"
+)
 
 func NewMux() http.Handler {
-	mux := http.NewServeMux()
+	// http.serveMux()は色々な問題点があるので、利用しない
+	// mux := http.NewServeMux()
+	// 代わりに chi を利用して、routerを記載する！
+	mux := chi.NewRouter()
+	// http.serveMux の記法は chi でも利用できる
 	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json; charset=utf-8")
 		_, _ = w.Write([]byte(`{"status": "ok"}`))
 	})
+
+	v := validator.New()
+	at := &handler.AddTask{Store: store.Tasks, Validator: v}
+	mux.Post("/tasks", at.ServeHTTP)
+	lt := &handler.ListTask{Store: store.Tasks}
+	mux.Get("/tasks", lt.ServeHTTP)
+
 	return mux
 }
